aaf4c520bc3a5a0351b07b95666234ac31e7755b
Your Name <you@example.com>
2026-05-07 21:09:01 +0300

    safe: site update

--- web/frontend/src/main.tsx
+++ web/frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { registerSW } from "virtual:pwa-register";
 import App from "./App";
 import "./index.css";
@@ -25,8 +25,8 @@ if (typeof window !== "undefined" && "serviceWorker" in navigator) {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
